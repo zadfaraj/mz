8199d5a73b4769a33d33b8b8e420ada5dfeab26f
zadfaraj <60322956+zadfaraj@users.noreply.github.com>
2020-01-27 22:27:14 +0330

    first init

---
+++ app/src/main/java/com/example/samplepro/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.samplepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        sharedPreferences =
+    }
+
+//    @Override
+    private void test(){
+
+    }
+}
